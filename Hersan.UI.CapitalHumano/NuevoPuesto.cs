605dd9fda9cab4d0c4816a480ace1310ff7611f2
MarcGreg <moisegreg30@gmail.com>
2019-06-19 11:05:16 -0500

    greg

--- Hersan.UI.CapitalHumano/NuevoPuesto.cs
+++ Hersan.UI.CapitalHumano/NuevoPuesto.cs
@@ -3,8 +3,6 @@ using Hersan.Entidades.Catalogos;
 using Hersan.Negocio;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
